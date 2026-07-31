d1fe629b8da968cc44e74fd3bfc6dea1c3c741fc
Kush Gupta <kushguptacse@gmail.com>
2026-07-31 04:40:40 +0000

    feat: implement streaming responses with Markdown rendering and update assistant system prompt

--- gardio_model_chat.py
+++ gardio_model_chat.py
@@ -5,10 +5,8 @@ import config
 openai.api_key = config.CHAT_COMPLETIONS_API_KEY
 openai.base_url = config.API_URL
 
-system_message ="You are an helpful ai assistant."
+system_message ="You are a helpful assistant that responds in markdown without code blocks"
 
-message_input = gr.Textbox(label="Your message:", info="Enter a message for GPT-4.1-mini", lines=7)
-message_output = gr.Textbox(label="Response:", lines=8)
 
 def message_gpt(message_input):
     response = openai.chat.completions.create(
@@ -20,12 +18,30 @@ def message_gpt(message_input):
     )
     return response.choices[0].message.content
 
+def stream_gpt(message_input):
+    stream_resp = openai.chat.completions.create(
+        model=config.LLM_MODEL_NAME,
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": message_input},
+        ],
+        stream=True,
+    )
+    result = ""
+    for chunk in stream_resp:
+        result += chunk.choices[0].delta.content or ""
+        yield result
+
+message_input = gr.Textbox(label="Your message:", info="Enter a message for chatting", lines=7)
+message_output = gr.Markdown(label="Response:")
+
 view = gr.Interface(
-    fn=message_gpt,
+    fn=stream_gpt,
     title="My Assistant", 
     inputs=[message_input], 
     outputs=[message_output], 
-    examples=["hello", "howdy"], 
+    examples=["Explain the Transformer architecture to a layperson",
+        "Explain the Transformer architecture to an aspiring AI engineer"],
     flagging_mode="never"
     )
 view.launch()
